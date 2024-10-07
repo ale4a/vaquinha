b19d90ca61aaa14fb7c120f0ce88f64e8342e0f4
Eber <doneber98@gmail.com>
2024-10-06 21:29:56 -0400

    feat(DetailsCard): create component

--- web/app/eber/page.tsx
+++ web/app/eber/page.tsx
@@ -1,8 +1,15 @@
 import SavingCard from '@/components/global/SavingCard/SavingCard';
+import DetailsCard from '../../components/global/DetailsCard/DetailsCard';
 
 const page = () => {
   return <div>
     <SavingCard name={'El Pasanaku'} amount={68} collateral={341} startIn='10-10-2024' peopleCount={3} period='montly' />
+    <br />
+    <hr />
+    <br />
+    <DetailsCard members={5} interestEarned={5} period='Monthly' finalized='06-10-2024' groupId='454474792930446547' name='Pasanaku' amount={68} />
+    <br />
+    <br />
   </div>;
 };
 
