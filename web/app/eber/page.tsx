9d678db9e9a18f53a97156393df8fbd2e1f5b824
Eber <doneber98@gmail.com>
2024-10-06 21:56:49 -0400

    feat(Table): Add component

--- web/app/eber/page.tsx
+++ web/app/eber/page.tsx
@@ -1,5 +1,40 @@
 import SavingCard from '@/components/global/SavingCard/SavingCard';
 import DetailsCard from '../../components/global/DetailsCard/DetailsCard';
+import Table from '@/components/global/Table/Table';
+import { item } from '@/components/global/Table/Table.types';
+
+const items: item[] = [
+  {
+    id: '0',
+    amount: 68,
+    paymentDeadline: '13-11-2024',
+    status: 'Paid'
+  },
+  {
+    id: '1',
+    amount: 69,
+    paymentDeadline: '14-11-2024',
+    status: 'Pending'
+  },
+  {
+    id: '2',
+    amount: 69,
+    paymentDeadline: '14-11-2024',
+    status: 'Pending'
+  },
+  {
+    id: '3',
+    amount: 69,
+    paymentDeadline: '14-11-2024',
+    status: 'Play Now'
+  },
+  {
+    id: '4',
+    amount: 69,
+    paymentDeadline: '14-11-2024',
+    status: 'Pending'
+  },
+]
 
 const page = () => {
   return <div>
@@ -8,6 +43,9 @@ const page = () => {
     <hr />
     <br />
     <DetailsCard members={5} interestEarned={5} period='Monthly' finalized='06-10-2024' groupId='454474792930446547' name='Pasanaku' amount={68} />
+    <br />
+    <br />
+    <Table items={items} />
     <br />
     <br />
   </div>;
