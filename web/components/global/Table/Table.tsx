dc48dd44ca47adfc48c5222168501798984aed28
Eber <doneber98@gmail.com>
2024-10-06 23:05:05 -0400

    styles(Table): improve styles of component

--- web/components/global/Table/Table.tsx
+++ web/components/global/Table/Table.tsx
@@ -2,31 +2,28 @@ import { Props } from './Table.types';
 
 export default function Table({ items }: Props) {
     return (
-        <div>
-            <table>
-                <tr>
-                    <th>Nro</th>
-                    <th>Amount</th>
-                    <th>Payment Deadline</th>
-                    <th>Status</th>
-                </tr>
-                <tbody className='bg-bg-200'>
-                    {
-                        items.map((item, i) => {
-                            return <tr key={item.id}>
-                                <td className='text-center'>{i + 1}</td>
-                                <td className='text-center'>{item.amount} asdf</td>
-                                <td className='text-center'>{item.paymentDeadline}</td>
-                                <td className='text-center'>
-                                    <div className='border rounded-xl py-1 px-3'>
-                                        {item.status}
-                                    </div>
-                                </td>
-                            </tr>
-                        })
-                    }
-                </tbody>
-            </table>
+        <div className='p-2'>
+            <div className='grid grid-cols-[45px_1fr_1fr_1fr] text-sm font-semibold text-center'>
+                <span>Nro</span>
+                <span>Amount</span>
+                <span>Payment Deadline</span>
+                <span>Status</span>
+            </div>
+            {
+                items.map((item, i) => {
+                    return <div className='grid grid-cols-[45px_1fr_1fr_1fr] py-2 bg-bg-200' key={item.id}>
+                        <span className='text-center'>{i + 1}</span>
+                        <span>{item.amount} USDT</span>
+                        <span>{item.paymentDeadline}</span>
+                        <span>
+                            <div className={'border rounded-xl py-1 px-3 mx-1 text-center' + `${item.status === 'Play Now' ? 'text-white border-transparent bg-success-green' : ''}`}
+                            >
+                                {item.status}
+                            </div>
+                        </span>
+                    </div>
+                })
+            }
         </div>
     )
 }
