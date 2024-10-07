5372612ba8e8e8c3624d53e38d2c49830026aad6
Alejandro Alvarez <a.alejandro.alvarez.a@gmail.com>
2024-10-06 21:09:30 -0400

    feat(font): add bold, light, semibold

--- web/components/global/Navbar/Navbar.tsx
+++ web/components/global/Navbar/Navbar.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar = ({ links }: { links: { label: string; path: string }[] }) => {
+  return (
+    <div className="bg-bg-100">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias iusto,
+      molestiae velit suscipit incidunt quaerat laboriosam facilis saepe
+      possimus minus optio, at ipsam aut, eius itaque qui maiores quo tenetur.
+      <p className="font-bold">abc</p>
+      <p className="font-light">xsz</p>
+      <p className="font-semibold">xsz</p>
+    </div>
+  );
 };
 
 export default Navbar;
