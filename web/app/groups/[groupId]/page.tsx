8006cf8d97d30e81df59681b9703a6a230ddf864
Eber <doneber98@gmail.com>
2024-10-07 21:43:35 -0400

    feat(group-id): add loading

--- web/app/groups/[groupId]/page.tsx
+++ web/app/groups/[groupId]/page.tsx
@@ -1,27 +1,43 @@
 'use client';
 import ButtonComponent from '@/components/global/ButtonComponent/ButtonComponent';
+import LoadingSpinner from '@/components/global/LoadingSpinner/LoadingSpinner';
 import Message from '@/components/message/Message';
 import BuildingStatus from '@/components/status/BuildingStatus';
 import Summary from '@/components/Summary/Summary';
 import { itemsSummary } from '@/components/Summary/Summary.types';
 // src/pages/groups/[idGroup].tsx
 import { usePathname } from 'next/navigation';
-import React from 'react';
+import React, { useState } from 'react';
 
 const GroupDetailPage = () => {
   const pathname = usePathname();    
+  const [isLoading, setIsLoading] = useState(false)
+
+  const doNextStep = async () => {
+    try {
+      setIsLoading(true)
+      await new Promise(resolve => setTimeout(resolve, 500)) // Only for simulate the fetch
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false)
+    }
+  }
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
 
   return (
     <div>
       <h1>Group Detail: {pathname}</h1>
       <Summary itemsSummary={itemsSummary} />
-      <BuildingStatus value1={true} value2={false} value3={false} />
+      <BuildingStatus value1={false} value2={false} value3={false} />
       <Message messageText={'It is necessary to deposit the collateral to ensure that each person can participate in the group, and to guarantee that everyone will pay appropriately'} />
       <div className='flex gap-2'>
         <ButtonComponent label='Back' type='secondary' size='large' onClick={() => {
           window.history.back()
         }} />
-        <ButtonComponent label='Deposit Collateral' type='primary' size='large' />
+        <ButtonComponent label='Deposit Collateral' type='primary' size='large' onClick={doNextStep} />
       </div>
     </div>
   );
