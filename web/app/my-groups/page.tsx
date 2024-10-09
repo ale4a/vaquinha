d7611f12e41a22f4cfe80ac25d083150826d0703
Alejandro Alvarez <a.alejandro.alvarez.a@gmail.com>
2024-10-09 02:20:49 -0400

    chore(my-groups): delete div tag

--- web/app/my-groups/page.tsx
+++ web/app/my-groups/page.tsx
@@ -30,7 +30,6 @@ const tabs = [
 const LoadingCard = () => (
   <div className="border border-accent-100 shadow rounded-md p-4 w-full mx-auto my-4">
     <div className="animate-pulse flex space-x-4">
-      {/*<div className="rounded-full bg-slate-200 h-10 w-10"></div>*/}
       <div className="flex-1 space-y-6 py-1">
         <div className="h-2 bg-slate-200 rounded"></div>
         <div className="space-y-3">
