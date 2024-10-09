9c24e1e812ac14c8f5ba6a587305727c21b5e6ca
raptor0929 <flaura.dev@gmail.com>
2024-10-09 02:58:38 -0400

    fix build

--- anchor/src/vaquinha-exports.ts
+++ anchor/src/vaquinha-exports.ts
@@ -1,18 +1,21 @@
-// Here we export some useful types and functions for interacting with the Anchor program.
-import { AnchorProvider, Program } from '@coral-xyz/anchor';
+import { AnchorProvider, Program, Idl } from '@coral-xyz/anchor';
 import { Cluster, PublicKey } from '@solana/web3.js';
+
+// Import the IDL as a module
 import VaquinhaIDL from '../target/idl/vaquinha.json';
+
+// Import the Vaquinha type
 import type { Vaquinha } from '../target/types/vaquinha';
 
 // Re-export the generated IDL and type
 export { Vaquinha, VaquinhaIDL };
 
 // The programId is imported from the program IDL.
-export const VAQUINHA_PROGRAM_ID = new PublicKey(VaquinhaIDL.address);
+export const VAQUINHA_PROGRAM_ID = new PublicKey(VaquinhaIDL.metadata.address);
 
 // This is a helper function to get the Vaquinha Anchor program.
 export function getVaquinhaProgram(provider: AnchorProvider) {
-  return new Program(VaquinhaIDL as Vaquinha, provider);
+  return new Program(VaquinhaIDL as Idl, VAQUINHA_PROGRAM_ID, provider);
 }
 
 // This is a helper function to get the program ID for the Vaquinha program depending on the cluster.
