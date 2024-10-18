7bd792e295280511ff7e6e8e26728b0dab994893
OscarGauss <me@oscargauss.com>
2024-10-17 23:33:14 -0400

    upd api

--- web/helpers/api.ts
+++ web/helpers/api.ts
@@ -1,5 +1,5 @@
 import { NEXT_PUBLIC_API } from '@/config/settings';
 
 export const includeApi = (route: string) => {
-  return `${NEXT_PUBLIC_API}/api/v1${route}`;
+  return `${NEXT_PUBLIC_API}/vaquita/api/v1${route}`;
 };
