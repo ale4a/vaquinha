fd5b2290c4ccfe73ec72715ddbae768c4715240f
Alejandro Alvarez <a.alejandro.alvarez.a@gmail.com>
2024-10-23 14:19:27 -0400

    fix

--- web/components/layout/ui-layout.tsx
+++ web/components/layout/ui-layout.tsx
@@ -12,7 +12,7 @@ declare global {
 }
 
 if (typeof window !== 'undefined') {
-  console.log('updateStartsOnTimestamp loaded :,D');
+  console.log('updateStartsOnTimestamp loaded :D');
   window.__TEST__ = {};
   window.__TEST__.updateStartsOnTimestamp = async (
     groupId: string,
