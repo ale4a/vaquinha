674c2f5fe40dd15352121df5d233373b50666e4c
Alejandro Alvarez <a.alejandro.alvarez.a@gmail.com>
2024-12-14 15:04:49 +0800

    fix(tailwind): add tailwind in vaquita-ui-submodule file

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   content: [
     join(
       __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+      '{src,pages,components,app,vaquita-ui-submodule}/**/*!(*.stories|*.spec).{ts,tsx,html}'
     ),
     ...createGlobPatternsForDependencies(__dirname),
     '../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
